fbbd3c0ada697e94b80f6c3073d19a3409d9a824
shaoyu <shaoyu>
2021-09-23 14:44:37 -0500

    update font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
--- frontend/src/components/LeftPanel/Assessment.js
+++ frontend/src/components/LeftPanel/Assessment.js
@@ -1,23 +1,21 @@
 import { useState } from "react";
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Typography, Box } from "@mui/material";
 
 import { assessments } from "../../resources/DigitalTwinConfigReorganized";
 
-const Assessment = () => {
-  const [selectedCategory, setSelectedCategory] = useState(assessments[0]);
+const AssessmentSelection = ({ selectedAssessment, setSelectedAssessment }) => {
   return (
     <Grid container>
       {Object.keys(assessments).map((value) => {
         return (
-          <Grid item xs={3}>
+          <Grid item xs={4} key={value}>
             <Button
               variant="contained"
               sx={{
-                textAlign: "center",
-                height: "40px",
+                height: "50px",
                 width: "100%",
               }}
-              onClick={() => setSelectedCategory(value)}
+              onClick={() => setSelectedAssessment(value)}
             >
               {value}
             </Button>
@@ -25,6 +23,54 @@ const Assessment = () => {
         );
       })}
     </Grid>
+  );
+};
+
+const AssessmentForm = ({ selectedAssessment }) => {
+  return (
+    <>
+      <Box sx={{ border: "1px solid black", width: "100%" }}>
+        <Typography variant="h6" gutterBottom component="div">
+          Current Score: 0
+        </Typography>
+      </Box>
+      <Grid container>
+        {assessments.SIRS.measurements.map((value) => {
+          return (
+            <Grid
+              item
+              xs={6}
+              sx={{
+                height: "100px",
+                borderStyle: "solid",
+                borderWidth: "1px",
+                borderColor: "black",
+              }}
+            >
+              <div>
+                {value?.name} {value?.unit === "" ? null : `(${value?.unit})`}
+              </div>
+              <div>Last updated time:</div>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
+
+const Assessment = () => {
+  const [selectedAssessment, setSelectedAssessment] = useState(
+    Object.keys(assessments)[0]
+  );
+  return (
+    <>
+      <Typography variant="h5" gutterBottom component="div">
+        Assessments
+      </Typography>
+      <AssessmentSelection {...{ selectedAssessment, setSelectedAssessment }} />
+      <AssessmentForm {...{ selectedAssessment }} />
+    </>
   );
 };
 
