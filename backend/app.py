6aab3424ab0bb71131032c670ac05936e9ea6def
shaoyu <shaoyu>
2021-09-29 13:41:25 -0500

    kommunication

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import simple_websocket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/k_comm", websocket=True)
+def k_comm():
+    ws = simple_websocket.Server(request.environ)
+    p = subprocess.Popen(['krun'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    try:
+        while True:
+            data=ws.receive()
+            p.stdin.write(data)
+            ws.send(data)
+    except simple_websocket.ConnectionClosed:
+        pass
+    return ''
+
+if __name__ == "__main__":
+    app.run()
