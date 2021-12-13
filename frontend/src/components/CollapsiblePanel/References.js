d46772d63db217a12aa9b746da8d54c9ab117d2f
Shaoyu <you@example.com>
2021-12-13 04:03:00 -0600

    push image files

--- frontend/src/components/CollapsiblePanel/References.js
+++ frontend/src/components/CollapsiblePanel/References.js
@@ -1,33 +1,63 @@
-import * as React from 'react';
-import TreeView from '@mui/lab/TreeView';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import TreeItem from '@mui/lab/TreeItem';
+import * as React from "react";
+import TreeView from "@mui/lab/TreeView";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import TreeItem from "@mui/lab/TreeItem";
+
+import image30 from "../../resources/guidelineimg/30.png";
 
 const references = {
-    "Sepsis Screening": ["Bucket-based Diagnosis Tree", "Bucket-Based Screening", "Vital Sign thresholds", "Septic Shock Screening"],
-    "Organ Dysfunction Assessments": ["Respiratory Dysfunction", "Renal Dysfunction", "Cardio Dysfunction", "Neurologic Dysfunction", "hematologic Dysfunction", "Hepatic Dysfunction"],
-    "Sepsis Bundle": ["Fluid Guideline", "Inotrope Guideline", "Antibiotic Guideline"],
-    "Additional Scores": ["PEWS", "Pediatric SIRS"]
-}
+  "Sepsis Screening": [
+    { name: "Bucket-based Diagnosis Tree", url: "" },
+    { name: "Bucket-Based Screening", url: "" },
+    { name: "Vital Sign thresholds", url: "" },
+    { name: "Septic Shock Screening", url: "" },
+  ],
+  "Organ Dysfunction Assessments": [
+    { name: "Respiratory Dysfunction", url: "" },
+    { name: "Renal Dysfunction", url: "" },
+    { name: "Cardio Dysfunction", url: "" },
+    { name: "Neurologic Dysfunction", url: "" },
+    { name: "hematologic Dysfunction", url: "" },
+    { name: "Hepatic Dysfunction", url: "" },
+  ],
+  "Sepsis Bundle": [
+    { name: "Fluid Guideline", url: "" },
+    { name: "Inotrope Guideline", url: "" },
+    { name: "Antibiotic Guideline", url: "" },
+  ],
+  "Additional Scores": [
+    { name: "PEWS", url: "" },
+    { name: "Pediatric SIRS", url: "" },
+  ],
+};
 
 export default function RichObjectTreeView() {
-    const renderTree = (nodes) => {return Object.keys(nodes).map(key => (
+  const renderTree = (nodes) => {
+    return Object.keys(nodes).map((key) => (
       <TreeItem key={key} nodeId={key} label={key}>
-        {
-          nodes[key].map(reference => { return (<TreeItem key={reference} nodeId={reference} label={reference} />) })
-        }
+        {nodes[key].map((reference) => {
+          return (
+            <TreeItem
+              key={reference.name}
+              nodeId={reference.name}
+              label={reference.name}
+            />
+          );
+        })}
       </TreeItem>
-    ))};
-  
-    return (
-      <TreeView
-        aria-label="rich object"
-        defaultCollapseIcon={<ExpandMoreIcon />}
-        defaultExpanded={['root']}
-        defaultExpandIcon={<ChevronRightIcon />}
-      >
-        {renderTree(references)}
-      </TreeView>
-    );
-  }
+    ));
+  };
+
+  return (
+    <TreeView
+      aria-label="rich object"
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpanded={["root"]}
+      defaultExpandIcon={<ChevronRightIcon />}
+    >
+      {renderTree(references)}
+      <img src={image30} alt="Logo" />;
+    </TreeView>
+  );
+}
