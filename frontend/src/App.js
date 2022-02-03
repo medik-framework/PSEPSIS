41fef1f1eb7e367393b708e91fb8810cf7ecd49c
Shaoyu <you@example.com>
2022-02-03 15:46:26 -0600

    add restore session

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import { useEffect, useState } from "react";
+
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -13,12 +15,16 @@ import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
 
 import PsepsisTablet from "./PsepsisTablet";
+import WelcomePage from "./WelcomePage"
 
 function App() {
+
+  const [isDisplayScreen, setIsDisplayScreen] = useState(false)
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <PsepsisTablet />
+        {isDisplayScreen ? <PsepsisTablet /> : <WelcomePage setIsDisplayScreen={setIsDisplayScreen} />}
       </ThemeProvider>
     </Provider>
   );
