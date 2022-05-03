8049a960f83781aeb25623d549bd83b785b90b53
shuangs3 <shuangs3@illinois.edu>
2022-05-03 16:56:20 -0500

    update gitignore

---
+++ mockpatient/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { EvaluationForm } from "./EvaluationForm";
+
+function App() {
+  return (
+    <div className="App">
+      <EvaluationForm />
+    </div>
+  );
+}
+
+export default App;
