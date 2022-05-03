68553d67a609294c1501ba694eb3799b4ea89a4f
shuangs3 <shuangs3@illinois.edu>
2022-05-03 16:50:04 -0500

    mock patient portal

--- sepsisform/src/App.js
+++ sepsisform/src/App.js
@@ -1,18 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import { EvaluationForm } from "./EvaluationForm";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <EvaluationForm />
-      </div>
-    </Provider>
-
+    <div className="App">
+      <EvaluationForm />
+    </div>
   );
 }
 
--- sepsisform/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import OrganDTReducer from './reducers'
-
-export default configureStore({
-  reducer: {
-    OrganDT: OrganDTReducer
-  }
-})
