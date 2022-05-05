4ed034be19c5ba3c10ca94965eea7528d4d10a6b
shuangs3 <shuangs3@illinois.edu>
2022-05-04 20:00:29 -0500

    complete delay logic, reorganize layout

--- mockpatient/src/App.js
+++ mockpatient/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import store from './store'
 import { Provider, useSelector, useDispatch } from 'react-redux'
 import { update, increment } from './organDataSlice'
 import { useInterval } from 'usehooks-ts'
 
-import { Button, Grid, Typography, TextField, Select, MenuItem } from "@mui/material";
+import { Button, Grid, Typography, TextField, Select, MenuItem, Card } from "@mui/material";
 import { OrganDTConfig } from "./resources/DigitalTwinConfigReorganized";
 
 const OrganSelection = ({ selectedDT, setSelectedDT }) => {
@@ -62,79 +62,146 @@ const OrganPage = ({ selectedDT }) => {
   )
 };
 
+
+
 const MeasurementNumeric = ({ organName, config }) => {
   const [inputValue, setInputValue] = useState(null);
   const [delay, setDelay] = useState(null);
-  const [delayTarget, setDelayTarget] = useState(null);
+  const [target, setTarget] = useState(null);
+  const [period, setPeriod] = useState(null);
+  const [elapse, setElapse] = useState(0);
+  const [step, setStep] = useState(0);
   const value = useSelector((state) => state.organData[organName][config.name]);
   const dispatch = useDispatch();
-  let   itvl = null;
-  let   nsteps = 0;
 
-  function gradualUpdate(startValue, targetValue, delay) {
-    const step = (targetValue - startValue)/delay;
-    const stepCount = (targetValue - startValue)/step;
-    itvl = setInterval(stepUpdate, 1000, step, stepCount, targetValue);
+  const resetInterval = () => {
+    setDelay(null);
+    setElapse(0);
   }
 
-  function stepUpdate(step, stepCount, targetValue) {
+  useEffect(() => {
+    if (delay && elapse === period){
+      resetInterval();
+      dispatch(update({value: target, organName: organName, measurementName: config.name}));
+    }
+      
+  }, [ delay, setDelay, elapse, setElapse, period, target, organName, config])
+
+  useInterval(() => {
     dispatch(increment({
       value: step, 
       organName: organName, 
       measurementName: config.name
     }));
-    nsteps++;
-    if (stepCount === nsteps) {
-      clearInterval(itvl);
-      nsteps = 0;
-      itvl = null;
-      dispatch(update({value: targetValue, organName: organName, measurementName: config.name}));
-    }
+    setElapse(elapse + 1);
+  }, delay);
+
+  const gradualUpdate = (startValue) => {
+    setStep((target - startValue)/period);
+    setDelay(1000);
   }
 
   return (
-    <Grid xs={4} item key={config.name}>
-        <Typography>
-            {config.name} {config.unit ? `(${config.unit})` : null}
+    <Grid xs={3} item spacing={1} key={config.name} 
+      sx={{ 
+        margin:'2px', 
+        backgroundColor:'#F0F8FF', 
+        border: '2px solid #6495ED',
+        borderRadius: '5px',
+      }}
+    >
+      <Grid xs={12}>
+        <Typography sx={{fontSize:'24px', padding:'1px'}}>
+            {config.name} {config.unit ? `(${config.unit})` : null}: {value ? +value.toFixed(2):null}
         </Typography>
-        <Typography>
-            Current value: {value ? +value.toFixed(2):null}
-        </Typography>
-        <TextField
+      </Grid>
+      <Grid container direction='row' xs={12} sx={{margin:'2px'}}>
+        <Grid item xs={3}>
+          <Typography sx={{fontSize:'16px'}}>
+              Instantly update to: 
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
             id={config.name}
-            variant="filled" 
+            sx={{fontSize:'18px', backgroundColor:'white'}}
+            variant="outlined" 
             type="number"
             onChange={(e) => setInputValue(e.target.value)}
-        />
-        <Button
-            onClick={() => {
-              dispatch(update({
-                value: Number(inputValue), 
-                organName: organName,
-                measurementName: config.name
-              }));
-            }}
-        >
-            Confrim
-        </Button>
-        <TextField
-            id='delay'
-            variant="filled" 
-            type="number"
-            onChange={(e) => setDelayTarget(Number(e.target.value))}
-        />
-        <TextField
-            id='target'
-            variant="filled" 
-            type="number"
-            onChange={(e) => setDelay(Number(e.target.value))}
-        />
-        <Button
-            disabled={isNaN(value)}
-            onClick={() => gradualUpdate(value, delayTarget, delay)}
-        >
-            delay
-        </Button>
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+              onClick={() => {
+                resetInterval();
+                dispatch(update({
+                  value: Number(inputValue), 
+                  organName: organName,
+                  measurementName: config.name
+                }));
+              }}
+          >
+              Confrim
+          </Button>
+        </Grid>
+      </Grid>
+      {!delay && <Grid container direction='row' xs={12} sx={{ margin:'2px' }}>
+        <Grid item xs={3}>
+          <Typography>
+              Gradually update to: 
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+              id='target'
+              variant="outlined"
+              sx={{fontSize:'18px', backgroundColor:'white'}}
+              type="number"
+              onChange={(e) => setTarget(Number(e.target.value))}
+          />
+        </Grid>
+        <Grid item xs={1}>
+          <Typography>
+              in
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+              id='period'
+              variant="outlined"
+              sx={{fontSize:'18px', backgroundColor:'white'}}
+              type="number"
+              onChange={(e) => setPeriod(Number(e.target.value))}
+          />
+        </Grid>
+        <Grid item xs={1}>
+          <Typography>
+              sec
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+              disabled={isNaN(value)}
+              onClick={() => gradualUpdate(value)}
+          >
+              Confirm
+          </Button>
+        </Grid>
+      </Grid>}
+      {delay&&<Grid container direction='row' xs={12} hidden={delay}>
+        <Grid item xs={9}>
+          <Typography>
+              Gradually updating to {target} in {period-elapse} secs.
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+              onClick={() => resetInterval()}
+          >
+              Cancel
+          </Button>
+        </Grid>
+      </Grid>}
     </Grid>
   )
 }
@@ -144,7 +211,14 @@ const MeasurementSelect = ({ organName, config }) => {
   const dispatch = useDispatch();
 
   return (
-    <Grid xs={4} item key={config.name}>
+    <Grid xs={3} item spacing={1} key={config.name} 
+      sx={{ 
+        margin:'2px', 
+        backgroundColor:'#F0F8FF', 
+        border: '2px solid #6495ED',
+        borderRadius: '5px',
+      }}
+    >
       <Typography>
           {config.name} {config.unit ? `(${config.unit})` : null}
       </Typography>
