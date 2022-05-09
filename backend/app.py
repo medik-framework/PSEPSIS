1cd5902ad1dfd442e9dd98335d73cc0edc79aa95
Manasvi Saxena <msaxena2@illinois.edu>
2022-05-09 17:20:15 -0500

    app.py:Add function to map frontend state to medik state

--- backend/app.py
+++ backend/app.py
@@ -35,19 +35,42 @@ def index3():
     global data_copy
     return data_copy
 
-def getValues(params):
-    result = {}
-    error = {}
-    if params == []:
-        error = { "code" : -32600, "message" : "*" }
-    for param in params:
-        if param in data_copy.keys():
-            result[param] = int(data_copy.get(param))
-        else:
-            error = { "code" : -32001, "message" : "Requested reading absent" }
 
-    return result, error
+    return {}
+    #result = {}
+    #error = {}
+    #if params == []:
+    #    error = { "code" : -32600, "message" : "*" }
+    #for param in params:
+    #    if param in data_copy.keys():
+    #        result[param] = int(data_copy.get(param))
+    #    else:
+    #        error = { "code" : -32001, "message" : "Requested reading absent" }
 
+    #return result, error
+
+
+def flatten_json(in_json):
+    return { "hr"                 : in_json["Cardiovascular"]["HR"]
+           , "sysBP"              : in_json["Cardiovascular"]["BP Sys"]
+           , "pulseQuality"       : in_json["Cardiovascular"]["Pulse Quality"]
+           , "capillaryRefill"    : in_json["Cardiovascular"]["Capillary Refill"]
+           , "temp"               : in_json["Immune"]["Temp"]
+           , "age"                : 60
+           , "highRiskConditions" : 0
+           , "skinCondition"      : in_json["Cardiovascular"]["Skin Color"]
+           , "mentalStatus"       : in_json["Neurologic"]["Behavior"] }
+
+@app.route("/get_values", methods=["POST", "GET"])
+def getValues():
+    global data_copy2
+    mapped_data = flatten_json(data_copy2)
+    requested_param = request.args.get("field_name")
+    print('requested param {}. Mapped data {}'.format(requested_param, mapped_data[requested_param]))
+    if requested_param in mapped_data.keys() and mapped_data[requested_param] != None:
+        return jsonify({'status': 'ok' , requested_param : mapped_data[requested_param]})
+    else:
+        return jsonify({'status': 'error' })
 
 @app.route("/", methods=["POST"])
 def index():
@@ -65,6 +88,7 @@ def index():
 def index2():
     global data_copy2
     json = request.json
+
     data_copy2 = json
     return ""
 
@@ -73,12 +97,14 @@ def submit():
     global data_copy2
     json_data = request.json
     data_copy2 = json_data
-    print(json.dumps(json_data, indent=4))
+    #print(json.dumps(json_data, indent=4))
     return ""
 
 @app.route("/debug")
 def debug_json():
     global data_copy2
+    flattened_json = flatten_json(data_copy2)
+    print(json.dumps(flattened_json, indent=4))
     return jsonify(data_copy2)
 
 
