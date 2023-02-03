ec5f6f262a0e3d96afa87ead7cce3ef85dd90a68
Jiale <jiale.ning@bytedance.com>
2023-02-02 20:34:55 -0600

    add storage at dialog config

--- frontend/src/resources/DialogConfig.js
+++ frontend/src/resources/DialogConfig.js
@@ -36,7 +36,12 @@ export const DialogConfig = {
         inputConfig: {
             label: "oxygen",
             type: "checklist",
-            options: ["Oxygen mask", "Oxygen nasal cannula", "Oxygen high flow cannula"],
+            options: [
+                "Oxygen mask",
+                "Oxygen nasal cannula", 
+                "Oxygen high flow cannula"
+            ],
+            storage: "logs/add"
         }
     },
     antibiotics: {
@@ -44,7 +49,15 @@ export const DialogConfig = {
         inputConfig: {
             label: "antibiotics",
             type:  "checklist",
-            options: ["Childre > 28 days normal hosts", "Children > 28 days immunosuppressed or at risk for infection with Pseudomonas sepcies", "Cannot receive pancillin or have recently received broad-spectrum antibiotics", "At risk of fungal infection", "Risk factors for rickettsial infection", "Infants 0 to 28 days"]
+            options: [
+                "Childre > 28 days normal hosts", 
+                "Children > 28 days immunosuppressed or at risk for infection with Pseudomonas sepcies", 
+                "Cannot receive pancillin or have recently received broad-spectrum antibiotics", 
+                "At risk of fungal infection", 
+                "Risk factors for rickettsial infection", 
+                "Infants 0 to 28 days"
+            ],
+            storage: "logs/add"
         }
     },
     culture: {
@@ -52,7 +65,13 @@ export const DialogConfig = {
         inputConfig: {
             label: "culture",
             type:  "checklist",
-            options: ["Urine culture obtained", "Blood culture obtained from venipuncture", "Blood culture obtained from line", "IV catheter culture obtained"]
+            options: [
+                "Urine culture obtained", 
+                "Blood culture obtained from venipuncture", 
+                "Blood culture obtained from line", 
+                "IV catheter culture obtained"
+            ],
+            storage: "logs/add"
         }
     },
     fluid: {
@@ -60,7 +79,13 @@ export const DialogConfig = {
         inputConfig: {
             label: "fluid",
             type:  "checklist",
-            options: ["Pulmonary Edema", "Renal Insufficiency", "History of Congenital Heart Disease", "None"]
+            options: [
+                "Pulmonary Edema", 
+                "Renal Insufficiency", 
+                "History of Congenital Heart Disease", 
+                "None"
+            ],
+            storage: "logs/add"
         }
     },
     history_and_general: {
@@ -68,7 +93,13 @@ export const DialogConfig = {
         inputConfig: {
             label: "history&general",
             type:  "checklist",
-            options: ["History of immunodeficiency?", "Chronic steroid use/adrenal insufficiency?", "Indwelling vascular catheter or other invasive?", "Pre-existing lung disease?"]
+            options: [
+                "History of immunodeficiency?", 
+                "Chronic steroid use/adrenal insufficiency?", 
+                "Indwelling vascular catheter or other invasive?", 
+                "Pre-existing lung disease?"
+            ],
+            storage: "logs/add"
         }
     },
     fluid_overload: {
@@ -76,7 +107,11 @@ export const DialogConfig = {
         inputConfig: {
             label: "fluid_overload",
             type:  "checklist",
-            options: ["Pulmonary Rales", "Hepatomegaly", "None"]
+            options: [
+                "Pulmonary Rales", 
+                "Hepatomegaly", 
+                "None"
+            ]
         }
     },
     showSepsisWarning: {
