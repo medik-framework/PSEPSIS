836eb4752261ef6a8fdd92f73a6bd8e49f32f262
Manasvi Saxena <msaxena2@illinois.edu>
2023-02-04 15:04:50 +0530

    Dockerfile:Update dockerfile and container run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name=frontend -it -v ~/Projects/PSEPSIS:/app/PSEPSIS -p 5000:5000  frontend_image:latest
+docker run --name=frontend -it -v ~/Projects/PSEPSIS:/app/PSEPSIS -p 5000:5000  frontend-with-k:latest
