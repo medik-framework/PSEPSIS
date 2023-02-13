8c4acce6002283c0304a0093d9da2b3cb8495d17
Manasvi Saxena <msaxena2@illinois.edu>
2023-02-13 17:34:15 -0600

    Update URIs to use docker container's ip

--- mockpatient/src/redux/miscSlice.js
+++ mockpatient/src/redux/miscSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = { apiURL: "http://127.0.0.1:5000/"}
+const initialState = { apiURL: "http://172.17.0.2:4002/" }
 
 export const miscSlice = createSlice({
   name: 'misc',
