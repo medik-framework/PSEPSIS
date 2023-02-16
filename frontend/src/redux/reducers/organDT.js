86b13e8c5ba036f99609973c03eb339acf2ae2ea
Manasvi Saxena <msaxena2@illinois.edu>
2023-02-16 15:24:53 -0600

    frontend/organDT.js:Fix accidentally included conflicts

--- frontend/src/redux/reducers/organDT.js
+++ frontend/src/redux/reducers/organDT.js
@@ -1,14 +1,5 @@
 import { OrganDTConfig } from "../../resources/DigitalTwinConfigReorganized";
-<<<<<<< HEAD
-import { createAction, createReducer } from '@reduxjs/toolkit'
-import { ConnectedTvOutlined } from "@mui/icons-material";
-import { act } from "react-dom/test-utils";
-
-const update = createAction("organDT/update");
-const update_all = createAction("organDT/update_all");
-=======
 import { createSlice } from '@reduxjs/toolkit'
->>>>>>> f9df0bb (finish digital twin)
 
 const initialState = {};
 OrganDTConfig.map((organ) => {
@@ -20,37 +11,6 @@ OrganDTConfig.map((organ) => {
     return [];
 });
 
-<<<<<<< HEAD
-const organDTReducer = createReducer(initialState, (builder) => {
-  builder
-      .addCase(update_all, (state, action) => {
-        const data = action.payload;
-        OrganDTConfig.map((organ) => {
-          state[organ.name] = data[organ.name]
-          return [];
-        });
-      })
-      .addCase(update, (state, action) => {
-        // const data = {[action.payload.measurement] :
-        //   {'value':action.payload.value, 'time':action.payload.timeStamp}
-        // }
-        // OrganDTConfig.map((organ) => {
-        //   const measurements = organ.measurements;
-        //   Object.keys(measurements).map((m) => {
-        //     if (m.name == action.payload.measurement) {
-        //       state[organ.name] = {...state[organ.name], ...data}
-        //     }
-        //   })
-        // })
-        //   state[organ.name] = action.payload[organ.name]
-        //   return [];
-        // });
-        // const data = action.payload
-        // console.log(data)
-        // console.log(state.toString())
-        // state[data.organ][data.measurement] = {'value': data.value, 'time':data.timeStamp}
-      })
-=======
 export const organDTSlice = createSlice({
   name: 'organDT',
   initialState: {...initialState},
@@ -66,7 +26,6 @@ export const organDTSlice = createSlice({
       return state
     },
   }
->>>>>>> f9df0bb (finish digital twin)
 });
 
 export const { update_all }  = organDTSlice.actions
