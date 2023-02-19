6bec5c4548fe907db62f1060fab02fa5e9b1064e
Manasvi Saxena <msaxena2@illinois.edu>
2023-02-18 19:30:25 -0600

    fronentend/*/reducers/endpoint.js:Add slice for handling connections callbacks

---
+++ frontend/src/redux/reducers/endpoints.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const endpointsSlice = createSlice({
+  name: 'endpoints',
+  initialState: {},
+  reducers: {
+    addEndpoint: (state, action) => {
+      state[action.payload.endointId] = action.payload.endpointHandlers
+    }
+  }
+});
+
+export const { addEndpoint } = endpointsSlice.actions
+export default endpointsSlice.reducer
