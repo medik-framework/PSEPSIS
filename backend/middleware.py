7e3b6991b066bdf27b5ed0afbf797911c26cb893
Manasvi Saxena <msaxena2@illinois.edu>
2023-02-27 17:55:37 -0600

    middleware:MedikHandler uses MedikWrapper for medik-io

--- backend/middleware.py
+++ backend/middleware.py
@@ -1,23 +1,31 @@
 from pathlib import Path
 from fractions import Fraction
 from data import OrganDt, Patient, DrugHist
+from backend_env import kompiled_dir, psepsis_pgm, set_env
+from wrapper import MedikWrapper
 
 import os
 import websockets
 
 import argparse, asyncio, concurrent, functools, json, logging, os, re, sys, threading, time
 
-base_dir     = Path(__file__).parents[0]
-psepsis_pgm  = base_dir     / 'psepsis.medik'
-medik_dir    = base_dir     / 'ext'          / 'medik-semantics'
-kompiled_dir = medik_dir    / '.build'       / 'llvm-exec'      / 'medik-llvm-kompiled'
-krelease_dir = medik_dir    / 'ext'          / 'k'              / 'k-distribution'      / 'target' / 'release' / 'k'
-kbin_dir     = krelease_dir / 'bin'
+#==== Common Helpers =====
 
-def set_env():
-    path_entires = [ kbin_dir ]
-    os.environ['PATH'] = str(kbin_dir.resolve()) \
-                                + os.pathsep + os.environ['PATH']
+def _broadcast(interface_id, event_name, event_args=[]):
+        return [{ 'id'        : interface_id
+                , 'action'    : 'broadcast'
+                , 'eventName' : event_name
+                , 'eventArgs' : event_args }]
+
+def _obtain_response(tid, interface_id, args):
+        return  [{ 'tid'       : tid
+                 , 'id'        : inteface_id
+                 , 'result'    : 'obtainResponse'
+                 , 'args'      : args }]
+
+def _sleep_response(tid):
+    return [{ 'action' : 'sleepReponse'
+            , 'tid'    : tid }]
 
 
 class StubProcess:
@@ -69,38 +77,12 @@ class StubProcess:
         self.feed_event.clear()
 
 
-class MedikProcess:
-    async def parse_json_stream(self, json_stream):
-        open_brace_count  = 0
-        close_brace_count = 0
-        last_scan_index   = 0
-        json_byte_str     = b''
-        while True:
-            logging.info("awaiting for json stream")
-            json_byte_str     += await json_stream.readuntil(b'}')
-            open_brace_count  += json_byte_str.count(b'{', last_scan_index)
-            close_brace_count += json_byte_str.count(b'}', last_scan_index)
-            if len(json_byte_str) == 0:
-                logging.info("return from parse_json_stream")
-                return None
-            if open_brace_count == close_brace_count:
-                obj = json.loads(json_byte_str)
-                json_byte_str       = b''
-                last_scan_index   = 0
-                open_proce_count  = 0
-                close_brace_count = 0
-                logging.info('read {}'.format(json.dumps(obj, indent=2)))
-                logging.info('json object: ' + str(obj))
-                return obj
-            last_scan_index = len(json_byte_str)
+class MedikHandler(MedikWrapper):
 
-
-    async def _sleep(self, k_process, duration, tid):
+    async def _sleep(self, duration, tid):
         logging.info('sleeping for duration {} for tid {}'.format(duration, tid))
         await asyncio.sleep(duration)
-        k_process.stdin.write(json.dumps({ 'tid'    : tid
-                                         , 'action' : 'sleepResponse' }, sort_keys=True).encode('utf-8'))
-        await k_process.stdin.drain()
+        await self.to_k_queue.put(_sleep_response(tid))
         logging.info('sleep complete message sent for tid {}'.format(tid))
 
     def process_rats(self, out_json):
@@ -117,109 +99,38 @@ class MedikProcess:
             out_json['args'] = processed_args
         return out_json
 
-    async def read_stdout(self, k_process):
-        try:
-            while True:
-                #print('awaiting input from json stream')
-                out_json = await self.parse_json_stream(k_process.stdout)
-                if out_json == None:
-                    break;
-                processed_json = self.process_rats(out_json)
-                match out_json.get('action'):
+    async def handle_from_k(self):
+        while True:
+            from_k_json = await self.from_k_queue.get()
+            if from_k_json == None:
+                break
+            processed_json = self.process_rats(from_k_json)
+            if 'action' in processed_json.keys():
+                match processed_json['action']:
                     case 'print':
-                        #print(*out_json['args'], end='', flush=True)
+                        print(*out_json['args'], end='', flush=True)
                     case 'sleep':
-                        sleep_task = asyncio.create_task(self._sleep( k_process
-                                                                    , out_json['duration']
-                                                                    , out_json['tid']))
-                    case 'exit':
-                        break
+                        asyncio.create_task(self._sleep( processed_json['duration']
+                                                       , processed_json['tid']))
+
+            if 'name' in processed_json.keys():
+                match processed_json['name']:
+                    case 'Obtain':
+                        raise RuntimeError('Obtain not implemented!')
                     case _:
-                        match out_json.get('name'):
-                            case 'Obtain':
-                                raise RuntimeError('Not Implemented Yet!')
-                            case _:
-                                #print('putting to from-k-queue -  {}'.format(json.dumps(out_json)))
-                                await self.to_app_queue.put(out_json)
-                                #print('put to from k queue successful')
+                        await self.to_app_queue.put(processed_json)
 
-        except asyncio.CancelledError:
-            return None
-
-
-    async def read_stderr(self, k_process):
-        try:
-            while True:
-                #print('awaiting for stderr')
-                err = await k_process.stderr.read()
-                if not err:
-                    break
-                else:
-                    sys.stderr.write(err.decode('utf-8'))
-                    sys.stderr.flush()
-        except asyncio.CancelledError:
-            #print('read std_err task cancelled')
-            return None
-
-
-    async def write_stdin(self, k_process):
-        try:
-            while True:
-                #print('awaiting for data to send to k')
-                in_data = await self.to_k_queue.get()
-                #print('sending to k: {}'.format(json.dumps(in_data, indent=2)))
-                k_process.stdin.write(json.dumps(in_data).encode('utf-8'))
-                await k_process.stdin.drain()
-                #print('sending to k complete')
-                if in_data.get('action') == 'exit':
-                    break
-        except asyncio.CancelledError:
-            return None
-
-    async def launch(self):
-        k_command = ( 'krun' , ['-d' , str(kompiled_dir.resolve())
-                               , '--output' , 'none'
-                               , str(psepsis_pgm.resolve()) ])
-        tasks = None
-        try:
-            k_process = await asyncio.create_subprocess_exec( k_command[0]
-                                                            , *k_command[1]
-                                                            , stdin=asyncio.subprocess.PIPE
-                                                            , stdout=asyncio.subprocess.PIPE
-                                                            , stderr=asyncio.subprocess.PIPE)
-
-            tasks = [asyncio.create_task(self.write_stdin(k_process), name='write-stdin')]
-            tasks += [asyncio.create_task(self.read_stdout(k_process), name='read-stdout')]
-            tasks += [asyncio.create_task(self.read_stderr(k_process), name='read-stderr')]
-
-            await asyncio.gather(*tasks)
-        except Exception as e:
-            #print(str(e))
-            for task in tasks:
-                if not task.cancelled():
-                    #print('cancelling task {}'.format(str(task.get_name())))
-                    task.cancel()
-
-
-    async def broadcast(self, interface_id, name, args):
-        await self.to_k_queue.put( { 'id'        : interface_id
-                                   , 'action'    : 'broadcast'
-                                   , 'eventName' : name
-                                   , 'eventArgs' : args })
-
-    def __init__(self, to_app_queue):
-        set_env()
+    def __init__(self, to_k_queue, from_k_queue, to_app_queue, kompiled_dir, psepsis_pgm):
+        super().__init__(to_k_queue, from_k_queue, kompiled_dir, psepsis_pgm)
         self.to_app_queue = to_app_queue
-        self.to_k_queue = asyncio.Queue()
 
 
 class AppProcess:
 
-    def __init__(self, ws_port, k_process, to_app_queue):
+    def __init__(self, ws_port, to_k_queue, to_app_queue):
         self.ws_port = ws_port
-        self.k_process = k_process
+        self.to_k_queue = to_k_queue
         self.interface_id = 'tabletApp'
-
         self.to_app_queue = to_app_queue
 
 
@@ -232,9 +143,10 @@ class AppProcess:
     async def from_app_handler(self, websocket):
         async for message in websocket:
             message_json = json.loads(message)
-            await k_process.broadcast( self.interface_id
-                                     , message_json['eventName']
-                                     , message_json.get('eventArgs', []))
+            await to_k_queue.put(_broadcast( self.interface_id
+                                           , message_json['eventName']
+                                           , message_json.get('eventArgs'
+                                                             , [])))
 
     async def setup_connections(self, websocket):
         to_app_task = asyncio.create_task(self.to_app_handler(websocket))
@@ -250,11 +162,12 @@ class AppProcess:
         async with websockets.serve(self.setup_connections, '0.0.0.0', self.ws_port):
             await asyncio.Future()
 
+
 class DataPortalProcess:
 
-    def __init__(self, ws_port, k_process, to_app_queue):
+    def __init__(self, ws_port, to_k_queue, to_app_queue):
         self.ws_port = ws_port
-        self.k_process = k_process
+        self.to_k_queue = to_k_queue
         self.to_app_queue = to_app_queue
 
         self.organ_dt = OrganDt()
@@ -308,11 +221,16 @@ if __name__ == "__main__":
                        , help='Path to stub containing simulate k messages' )
 
     args = parser.parse_args()
+
+    from_k_queue = asyncio.Queue()
+    to_k_queue = asyncio.Queue()
     to_app_queue = asyncio.Queue()
+
     if args.stub:
         k_process = StubProcess(args.stub, to_app_queue)
     else:
-        k_process = MedikProcess(to_app_queue)
-    app_process = AppProcess(args.user_port, k_process, to_app_queue)
-    portal_process = DataPortalProcess(args.data_port, k_process, to_app_queue)
+        set_env()
+        k_process = MedikHandler(to_k_queue, from_k_queue, to_app_queue, kompiled_dir, psepsis_pgm)
+    app_process = AppProcess(args.user_port, to_k_queue, to_app_queue)
+    portal_process = DataPortalProcess(args.data_port, to_k_queue, to_app_queue)
     asyncio.run(main(app_process, k_process, portal_process))
