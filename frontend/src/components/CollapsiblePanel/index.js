ac1ae93d25b9b50145fa3fde32790668f986b47c
Shuang <shuangs3@illinois.edu>
2023-02-28 17:50:21 -0600

    frontend/LineGraph:Implement fluid response line graph in collapsible panel

--- frontend/src/components/CollapsiblePanel/index.js
+++ frontend/src/components/CollapsiblePanel/index.js
@@ -5,6 +5,7 @@ import { Button, Grid, Popover } from "@mui/material";
 import WorkflowTab from "./WorkflowTab";
 import Checklist from "./Checklist";
 import Reference from "./References";
+import LineGraph from "./LineGraph";
 
 const CollapsiblePanel = () => {
   const [selectedButton, setSelectedButton] = useState();
@@ -12,7 +13,7 @@ const CollapsiblePanel = () => {
   const anchorEl = useRef(null);
 
   //const buttons = ["history", "flowchart", "checklists", "references"];
-  const buttons = ["flowchart", "checklists", "references"];
+  const buttons = ["flowchart", "checklists", "references", "fluid response"];
 
   return (
     <div style={{ height: "100vh" }} ref={anchorEl}>
@@ -52,6 +53,7 @@ const CollapsiblePanel = () => {
           {selectedButton === "flowchart" && <WorkflowTab />}
           {selectedButton === "checklists" && <Checklist />}
           {selectedButton === "references" && <Reference />}
+          {selectedButton === "fluid response" && <LineGraph />}
         </div>
       </Popover>
     </div>
