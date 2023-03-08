f94d3cce4d623298de2b3408ac57fde5e95c7dba
Manasvi Saxena <msaxena2@illinois.edu>
2023-03-08 13:31:33 -0600

    frontend/Dialogs*:implement fluid therapy logic

--- frontend/src/components/DialogContent/InputDialog.js
+++ frontend/src/components/DialogContent/InputDialog.js
@@ -17,6 +17,8 @@ const InputContent = (args) => {
       return <NumericInput {...args}/>
     case "checklist":
       return <Checklist {...args}/>
+    case "plain":
+      return <>{args.originalArgs[1]}</>
     default:
       return <></>
   }
@@ -44,33 +46,33 @@ const InputDialog = ({ open, setOpen, info }) => {
     }
   }, [retDict, storeDict, info])
 
+  const makeKMessage = (inputConfig, retDict) => {
+    let data = {
+      'id':counter,
+      'source': 1,
+      "timestamp": Date.now(),
+      "eventArgs": Object.values(retDict),
+    }
+    if (Object.keys(inputConfig).includes('eventName')){
+      data.eventName =  config.inputConfig.eventName
+    }
+    return data
+  }
+
   const handleContinue = () => {
-    if(info.id !== undefined){
-      const data = {
-        'id':counter,
-        'source': 1,
-        'response_to':info.id,
-        "need_response": false,
-        "timestamp": Date.now(),
-        "eventName": config.inputConfig.eventName,
-        "eventArgs": Object.values(retDict),
-      }
-      kEndpoint.sendMessage(JSON.stringify(data));
-      dispatch({type: "dialogs/setDone", payload: data});
-    } else {
-      const data = {
-        'source': 1,
-        "need_response": false,
-        "timestamp": Date.now(),
-        "eventName": config.inputConfig.eventName,
-        "eventArgs": Object.values(retDict),
-      }
+    const data = makeKMessage(inputConfig, retDict);
+    if (Object.keys(inputConfig).includes('eventName')){
       kEndpoint.sendMessage(JSON.stringify(data));
     }
-    dispatch({
-      type: inputConfig.storage,
-      payload: storeDict
-    });
+    if(info.id) {
+      dispatch({type: "dialogs/setDone", payload: data});
+    }
+    if(inputConfig.storage) {
+      dispatch({
+        type: inputConfig.storage,
+        payload: storeDict
+      });
+    }
     setOpen(false);
   }
 
@@ -78,7 +80,12 @@ const InputDialog = ({ open, setOpen, info }) => {
     <Dialog open={open}>
       <DialogTitle>{config.title}</DialogTitle>
       <DialogContent>
-        <InputContent {...{ inputConfig, setStoreDict, setRetDict }}/>
+        <InputContent {...{
+          inputConfig:  inputConfig,
+          setStoreDict: setStoreDict,
+          setRetDict:   setRetDict,
+          originalArgs: info.args
+        }}/>
       </DialogContent>
       <DialogActions>
         <Button
