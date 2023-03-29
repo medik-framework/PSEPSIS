e048ea0a5e944f01486cf131a865b51ec541ba89
Manasvi Saxena <msaxena2@illinois.edu>
2023-03-29 15:55:58 -0500

    tests/shock-screening:Add tests for shock screening

--- backend/backend_env.py
+++ backend/backend_env.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 import logging, os
 
-base_dir        = Path(__file__).parents[0]
-guidelines_pgm  = base_dir     / 'guidelines.medik'
-driver_pgm      = base_dir     / 'driver.medik'
-ghosts_pgm      = base_dir     / 'ghosts.medik'
-medik_dir       = base_dir     / 'ext'          / 'medik-semantics'
-kompiled_dir    = medik_dir    / '.build'       / 'llvm-exec'      / 'medik-llvm-kompiled'
-krelease_dir    = medik_dir    / 'ext'          / 'k'              / 'k-distribution'      / 'target' / 'release' / 'k'
-kbin_dir        = krelease_dir / 'bin'
+base_dir                  = Path(__file__).parents[0]
+guidelines_pgm            = base_dir     / 'guidelines.medik'
+driver_pgm                = base_dir     / 'driver.medik'
+screening_only_driver_pgm = base_dir     / 'screening_only_driver.medik'
+ghosts_pgm                = base_dir     / 'ghosts.medik'
+medik_dir                 = base_dir     / 'ext'          / 'medik-semantics'
+kompiled_dir              = medik_dir    / '.build'       / 'llvm-exec'      / 'medik-llvm-kompiled'
+krelease_dir              = medik_dir    / 'ext'          / 'k'              / 'k-distribution'      / 'target' / 'release' / 'k'
+kbin_dir                  = krelease_dir / 'bin'
 
 def set_env():
     if os.getenv('LOGINFO'):
