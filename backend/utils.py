59f437be7d4f9482f11beed83683e599aa729a96
Shuang <shuangs3@illinois.edu>
2023-05-15 15:39:53 -0500

    backend:support frontend to use combined guideline files

--- backend/utils.py
+++ backend/utils.py
@@ -4,6 +4,15 @@ from fractions import Fraction
 
 rat_re = re.compile(r'\<(-?\d+),(\d+)\>Rat')
 
+def combined_temp_file(tmp_path, filepaths, combined_file_name):
+    combined_pgm_path = tmp_path / combined_file_name
+    combined_pgm = ''
+    for filepath in filepaths:
+        with filepath.open() as handler:
+            combined_pgm += handler.read()
+    combined_pgm_path.write_text(combined_pgm)
+    return combined_pgm_path
+
 def json_rat_strs_to_fractions(in_json):
     match in_json:
         case list():
