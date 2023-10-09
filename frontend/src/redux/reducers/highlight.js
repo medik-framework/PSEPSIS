3da21844e7154e836660b4ef4985cdceb7880a43
Pradhyumna P <pradhyumna_p@Pradhyumnas-MacBook-Pro.local>
2023-10-09 14:19:32 -0500

    Add frontend changes for multi-highlight

--- frontend/src/redux/reducers/highlight.js
+++ frontend/src/redux/reducers/highlight.js
@@ -3,7 +3,9 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
   selectedMedicationTab: 0,
   highlightedMedication: [],
-  suggestedDosage: []
+  suggestedDosage: [],
+  highlightedMedicationPairs: {},
+  suggestedDosagePairs: {}
 }
 
 export const highlightSlice = createSlice({
@@ -15,10 +17,33 @@ export const highlightSlice = createSlice({
       state.highlightedMedication.push(action.payload.medication);
       state.suggestedDosage.push(action.payload.dose);
     },
+    setHighlights: (state, action) => {
+      const { tab, medication, dose } = action.payload;
+      if (!state.highlightedMedicationPairs[tab]) {
+        state.highlightedMedicationPairs[tab] = [];
+      }
+      if (!state.suggestedDosagePairs[tab]) {
+        state.suggestedDosagePairs[tab] = [];
+      }
+      state.selectedMedicationTab = action.payload.tab;
+      state.highlightedMedicationPairs[tab].push(medication);
+      state.suggestedDosagePairs[tab].push(dose);
+    },
     unsetHighlight: (state, action) => {
       const idx = state.highlightedMedication.indexOf(action.payload);
+      if(idx !== -1){
       state.highlightedMedication.splice(idx,1);
       state.suggestedDosage.splice(idx,1);
+      }
+
+      const targetValue = action.payload;
+      for (const key in state.highlightedMedicationPairs) {
+        if (state.highlightedMedicationPairs[key].includes(targetValue)) {
+          delete state.highlightedMedicationPairs[key];
+          delete state.suggestedDosagePairs[key];
+          break;
+        }
+      }
     },
     setMedicationTab: (state, action) => {
       state.selectedMedicationTab = action.payload.tab;
@@ -26,6 +51,6 @@ export const highlightSlice = createSlice({
   }
 })
 
-export const { setHighlight, unsetHighlight, setMedicationTab } = highlightSlice.actions
+export const { setHighlight, setHighlights, unsetHighlight, setMedicationTab } = highlightSlice.actions
 
 export default highlightSlice.reducer
