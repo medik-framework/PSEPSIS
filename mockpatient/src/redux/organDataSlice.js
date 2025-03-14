54bebf45184e65548a8112a9ab967847ec9572a1
Praveen Vijayakumar <pv16@illinois.edu>
2025-03-14 11:00:28 -0500

    redux: move gradual update parameters to redux

--- mockpatient/src/redux/organDataSlice.js
+++ mockpatient/src/redux/organDataSlice.js
@@ -1,11 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { OrganDTConfig } from "../resources/DigitalTwinConfigReorganized";
 
-const initialState = {}
+const initialState = {};
 OrganDTConfig.map((organ) => {
     const measurements = organ.measurements;
     initialState[organ.name] = Object.keys(measurements).reduce((prev, key) => {
-        prev[measurements[key].name] = null;
+        prev[measurements[key].name] = {
+            value: null,
+            target: null,
+            period: null,
+            step: 0,
+            elapse: 0,
+            delay: null
+        };
         return prev;
     }, {});
     return [];
@@ -16,23 +23,43 @@ export const organDataSlice = createSlice({
   initialState: { ...initialState },
   reducers: {
     update: (state, action) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state[action.payload.organName][action.payload.measurementName] = action.payload.value
+      state[action.payload.organName][action.payload.measurementName].value = action.payload.value;
+      
     },
     increment: (state, action) => {
-      state[action.payload.organName][action.payload.measurementName] += action.payload.value
+      state[action.payload.organName][action.payload.measurementName].value += action.payload.value;
+      state[action.payload.organName][action.payload.measurementName].elapse += 1;
     },
-    reset: (state, action) => {
-      console.log("reset");
-      state = initialState;
+    setGradualUpdate: (state, action) => {
+      const { organName, measurementName, target, period, step, delay } = action.payload;
+      state[organName][measurementName].target = target;
+      state[organName][measurementName].period = period;
+      state[organName][measurementName].step = step;
+      state[organName][measurementName].delay = delay;
+      state[organName][measurementName].elapse = 0; 
+    },
+    incrementElapse: (state, action) => {
+      const { organName, measurementName } = action.payload;
+      
+      if (!state[organName] || !state[organName][measurementName]) {
+          console.error(`Error: ${organName}.${measurementName} not found in Redux state.`);
+          return;
+      }
+
+      state[organName][measurementName].elapse += 1;
+    },
+    resetGradualUpdate: (state, action) => {
+      
+      const { organName, measurementName } = action.payload;
+      
+      state[organName][measurementName].target = 0;
+      state[organName][measurementName].period = 0;
+      state[organName][measurementName].step = 0;
+      state[organName][measurementName].delay = 0;
+      state[organName][measurementName].elapse = 0;
     }
   },
 })
 
-// Action creators are generated for each case reducer function
-export const { update, increment, reset } = organDataSlice.actions
-
-export default organDataSlice.reducer
+export const { update, increment, setGradualUpdate, incrementElapse, resetGradualUpdate } = organDataSlice.actions;
+export default organDataSlice.reducer;
--- mockpatient/src/redux/store.js
+++ mockpatient/src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import OrganDTReducer from './organDataSlice'
 import MiscReducer from './miscSlice'
+// import { configureStore } from "@reduxjs/toolkit";
+// import { persistStore, persistReducer, createTransform } from "redux-persist";
+// import storage from "redux-persist/lib/storage"; // Uses localStorage
+// import { combineReducers } from "redux";
 
 export default configureStore({
   reducer: {
     OrganDT: OrganDTReducer,
     misc: MiscReducer
   }
-})
+});
